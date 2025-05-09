880534efe2b194dbc3215ed14f239027d243ff30
yodyaa <yodyaa2024@gmail.com>
2025-05-09 22:47:42 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,13 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+  // Firebase config
+  const firebaseConfig = {
   apiKey: "AIzaSyBt0ikC_4GPLgJOeth-CzPIq_9V_jDKmLQ",
   authDomain: "inventory-f0daa.firebaseapp.com",
   projectId: "inventory-f0daa",
   storageBucket: "inventory-f0daa.firebasestorage.app",
   messagingSenderId: "257202949333",
   appId: "1:257202949333:web:1e0ed0f840b51f874c265c",
-  measurementId: "G-BHBRN5PECH"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  measurementId: "G-BHBRN5PECH"    };
+  
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
+  const storage = firebase.storage().ref();
