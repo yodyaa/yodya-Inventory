a87679bb3645933123729e1753fe513fc66d17e0
yodyaa <yodyaa2024@gmail.com>
2025-06-14 00:27:15 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
-
   // Firebase config
   const firebaseConfig = {
       apiKey: "AIzaSyBt0ikC_4GPLgJOeth-CzPIq_9V_jDKmLQ",
       authDomain: "inventory-f0daa.firebaseapp.com",
       databaseURL: "https://inventory-f0daa-default-rtdb.asia-southeast1.firebasedatabase.app",
       projectId: "inventory-f0daa",
-      storageBucket: "inventory-f0daa.firebasestorage.app",
+      storageBucket: "inventory-f0daa.appspot.com", 
       messagingSenderId: "257202949333",
       appId: "1:257202949333:web:1e0ed0f840b51f874c265c",
-      measurementId: "G-BHBRN5PECH"  };
+      measurementId: "G-BHBRN5PECH"
+  };
   
   firebase.initializeApp(firebaseConfig);
   const db = firebase.database();
